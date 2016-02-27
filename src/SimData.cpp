bb551d97f3ddb4e313485fcca57c85681d5f8c17
djbuldog <djbuldog@debian>
2016-02-27 03:39:23 +0100

    * checkpoint - fixed, cleaned, working

--- src/SimData.cpp
+++ src/SimData.cpp
@@ -7,72 +7,20 @@
 
 #include "serial.h"
 
-class DataRefOld {
-	int last;
-
-	protected:
-		bool changed;
-		XPLMDataRef ptr;
-		std::string ser_code;
-		const std::string getValStr();
-		
-	public:
-		DataRefOld(): changed(false), ptr(NULL) {}
-		DataRefOld(const std::string name, const std::string ser_code): last(0) { assign(name, ser_code); }
-		void assign(const std::string name, const std::string ser_code);
-		bool isChanged() { return changed; };
-		bool isInit() { return (ptr!=NULL); }
-		void update();
-		const std::string getSerStr();
-		
-};
-
-/* ******************************************************************* */
-
-void DataRefOld::assign(const std::string name, const std::string ser_code) {
-	this->ser_code = ser_code;
-	ptr = XPLMFindDataRef(name.c_str());
-	changed = false;
-}
-
-const std::string DataRefOld::getSerStr() {
-	this->changed = false;
-	std::string retval(ser_code);
-	return retval + " " + getValStr() + '\n';
-}
-
-void DataRefOld::update() {
-
-	if (ptr == NULL) return;
-
-	int val = XPLMGetDatai(this->ptr);
-	if (val!=this->last) {
-		changed = true;
-		last=val;
-	}  
-}
-
-const std::string DataRefOld::getValStr() {
-	std::stringstream ss;
-	ss << this->last;
-  return ss.str();
-}
-
-/*
 class DataRef {
 
 	protected:
 		bool changed;
 		XPLMDataRef ptr;
 		std::string ser_code;
-		virtual const std::string getValStr();
+		virtual const std::string getValStr() =0;
 		
 	public:
 		DataRef(): changed(false), ptr(NULL) {}
 		void assign(const std::string name, const std::string ser_code);
 		bool isChanged() { return changed; };
 		bool isInit() { return (ptr!=NULL); }
-		virtual void update();
+		virtual void update() =0;
 		const std::string getSerStr();
 		
 };
@@ -102,10 +50,10 @@ class DataRefFloat : public DataRef {
 		const std::string getSerStr();
 };
 
-*/
 /* ******************************************************************* */
-/*
+
 void DataRef::assign(const std::string name, const std::string ser_code) {
+	//std::cerr << "LCDxpl: Called assign with " << name << ", " << ser_code << std::endl;
 	this->ser_code = ser_code;
 	ptr = XPLMFindDataRef(name.c_str());
 	changed = false;
@@ -114,11 +62,11 @@ void DataRef::assign(const std::string name, const std::string ser_code) {
 const std::string DataRef::getSerStr() {
 	this->changed = false;
 	std::string retval(ser_code);
-	return retval + " " + getValStr();
+	return retval + " " + getValStr() + '\n';
 }
-*/
+
 /* ******************************************************************* */
-/*
+
 void DataRefInt::update() {
 
 	if (ptr == NULL) return;
@@ -135,9 +83,9 @@ const std::string DataRefInt::getValStr() {
 	ss << this->last;
   return ss.str();
 }
-*/
+
 /* ******************************************************************* */
-/*
+
 void DataRefFloat::update() {
 
 	if (ptr == NULL) return;
@@ -154,12 +102,11 @@ const std::string DataRefFloat::getValStr() {
 	ss << this->last;
   return ss.str();
 }
-*/
+
 /* ******************************************************************* */
 
 /* We keep our data ref globally since only one is used for the whole plugin. */
-//static std::vector<DataRef> datarefs;
-static std::vector<DataRefOld> datarefs;
+static std::vector<DataRef*> datarefs;
 static serial ser;
 static float callback_rate = -1.0;
 
@@ -172,30 +119,33 @@ PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc) {
 	strcpy(outName, "LCDxpl");
 	strcpy(outSig, "bari2.eu");
 	strcpy(outDesc, "A plugin for X-Plane LCD.");
-/*
-	datarefs.push_back(DataRefInt("sim/cockpit2/radios/actuators/transponder_code","SQK"));
-	datarefs.push_back(DataRefInt("sim/cockpit2/radios/actuators/adf1_frequency_hz","ADF"));
-*/	
-	datarefs.push_back(DataRefOld("sim/cockpit2/radios/actuators/transponder_code","SQK"));
-	datarefs.push_back(DataRefOld("sim/cockpit2/radios/actuators/adf1_frequency_hz","ADF"));
 
-	XPLMRegisterFlightLoopCallback(FlightLoopCallback,callback_rate,NULL);
+	datarefs.push_back(new DataRefInt("sim/cockpit2/radios/actuators/transponder_code","SQK"));
+	datarefs.push_back(new DataRefInt("sim/cockpit2/radios/actuators/adf1_frequency_hz","ADF"));
 
 	/* Only return that we initialized correctly if we found the data ref. */
-/*	
-	for(std::vector<DataRef>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
-		if (!it->isInit()) return 0;
+
+	for(std::vector<DataRef*>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
+		if (!(*it)->isInit()) {
+			std::cerr << "LCDxpl: Some dataref obj is not initialized!!" << std::endl;
+			return 0;
+		}
 	}
-*/
-	for(std::vector<DataRefOld>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
-		if (!it->isInit()) return 0;
-	}
+
+	XPLMRegisterFlightLoopCallback(FlightLoopCallback,callback_rate,NULL);
 
 	return 1;
 }
 
 PLUGIN_API void	XPluginStop(void) {
+
   XPLMUnregisterFlightLoopCallback(FlightLoopCallback, NULL);
+  
+	for(std::vector<DataRef*>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
+    delete *it;
+	}
+	datarefs.clear();
+
 }
 
 PLUGIN_API void XPluginDisable(void) {
@@ -209,19 +159,11 @@ PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFromWho, int inMessage, voi
 }
 
 float FlightLoopCallback(float inElapsedSinceLastCall, float inElapsedTimeSinceLastFlightLoop, int inCounter, void* inRefcon) {
-/*
-	for(std::vector<DataRef>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
-		it->update();
-		if (it->isChanged()) {
-			ser.write(it->getSerStr());
-		}
-	}
-*/
 
-	for(std::vector<DataRefOld>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
-		it->update();
-		if (it->isChanged()) {
-			ser.write(it->getSerStr());
+	for(std::vector<DataRef*>::iterator it = datarefs.begin(); it != datarefs.end(); ++it) {
+		(*it)->update();
+		if ((*it)->isChanged()) {
+			ser.write((*it)->getSerStr());
 		}
 	}
 
